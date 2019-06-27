6cbdd68c1c161b7c8c5f2c515ef17109c0d4846a
soeunj <thdmsdia@gmail.com>
2019-06-27 09:53:36 +0300

    Add comments

--- api.py
+++ api.py
@@ -5,10 +5,10 @@ import json
 
 app = flask.Flask(__name__)
 # mongodb config
+# If you prefer to use different database, change below database name
 app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 mongo = PyMongo(app)
 
-# route
 @app.route('/median_pickup_time', methods=['GET'])
 def median_pickup_time():
     # params
@@ -19,6 +19,7 @@ def median_pickup_time():
     pipeline = [{"$match":{"location_id":int(location_id),"iso_8601_timestamp":{"$gte":start_time, "$lte":end_time}}},\
         {"$group":{"_id":"$location_id", "median":{"$avg":"$pickup_time"}}}]
     result = {}
+    # If you prefer to use different collection, change below collection name
     for r in mongo.db.pickup_time.aggregate(pipeline):
         result['median'] = int(r['median'])
     res = json.dumps(result)
